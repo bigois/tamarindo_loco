4cd6bb5ece6bc7f216642548ed36365ba932014f
Guilherme Bigois <guilhermebigois@outlook.com>
2018-11-05 22:23:14 -0200

    Adicionada a tela de configuração com TableView

--- TestDrive/TestDrive/Views/DetalhePage.xaml.cs
+++ TestDrive/TestDrive/Views/DetalhePage.xaml.cs
@@ -6,6 +6,31 @@ using Xamarin.Forms.Xaml;
 namespace TestDrive.Views {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DetalhePage : ContentPage {
+        // DECLARAÇÃO DE CONSTANTES
+        private const Double FREIO_ABS = 800.0;
+        private const Double AR_CONDICIONADO = 1000.0;
+        private const Double MP3_PLAYER = 500.0;
+
+        // MÉTODO FEIO DE FORMTAÇÃO DE TEXTO DO SWITCH FREIO ABS
+        public String TextFreioABS {
+            get {
+                return String.Format("Freio ABS - R$ {0:N}", FREIO_ABS);
+            }
+        }
+
+        // MÉTODO FEIO DE FORMATAÇÃO DE TEXTO DO SWITCH AR CONDICIONADO
+        public String TextArCondicionado {
+            get {
+                return String.Format("Ar Condicionado - R$ {0:N}", AR_CONDICIONADO);
+            }
+        }
+
+        public String TextMP3Player {
+            get {
+                return String.Format("MP3 Player - R$ {0:N}", MP3_PLAYER);
+            }
+        }
+
         // PROPRIEDADE GLOBAL PARA FACILITAR O TRANSPORTE
         public Veiculo Veiculo { get; set; }
 
@@ -18,11 +43,31 @@ namespace TestDrive.Views {
             // ALTERA O TÍTULO COM BASE NO NOME DO VEÍCULO
             Title = veiculo.Nome;
 
+            TextTotal.Text = String.Format("Valor Total: R$ {0:N}", Veiculo.Preco + FREIO_ABS + MP3_PLAYER);
+
             // VINCULA O MÉTODO DE CLIQUE COM A AÇÃO DE CLIQUE NO BOTÃO
             ButtonProximo.Clicked += ButtonProximoClicked;
 
             // DEFINE ESTA CLASSE COMO CONTEXTO DE BINDING
             BindingContext = this;
+
+            // ADICIONA AÇÕES EM CASO DE MUDANÇA DE ESTADO DOS SWITCHES
+            SwitchFreio.OnChanged += OnSwitchChanged;
+            SwitchMP3.OnChanged += OnSwitchChanged;
+            SwitchArCondicionado.OnChanged += OnSwitchChanged;
+        }
+
+        // MÉTODO PARA MUDANÇA DE SWITCHES
+        private void OnSwitchChanged(Object sender, ToggledEventArgs e) {
+            Double ValorTotal = Veiculo.Preco;
+
+            // VERIFICA SE OS SWITCHES ESTÃO SELECIONADOS,
+            // CASO SIM, SOMA OS VALORES
+            if (SwitchFreio.On) { ValorTotal += FREIO_ABS; }
+            if (SwitchMP3.On) { ValorTotal += MP3_PLAYER; }
+            if (SwitchArCondicionado.On) { ValorTotal += AR_CONDICIONADO; }
+
+            TextTotal.Text = String.Format("Valor Total: R$ {0:N}", ValorTotal);
         }
 
         // AÇÃO DO BOTÃO DE NAVEGAÇÃO PARA A PÁGINA AGENDAMENTO
